362b3c5693a0c32064e2e0ba6b1060daa6313971
MoonlightDreamer <dase.vodemago@yandex.ru>
2023-01-02 22:23:16 +0300

    adding spring boot application

---
+++ src/main/java/ru/dgu/dockerproject/DockerProjectSpringBootApplication.java
@@ -0,0 +1,11 @@
+package ru.dgu.dockerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerProjectSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DockerProjectSpringBootApplication.class);
+    }
+}
